f68fd96954946d0df9b6dd556da0da3f48e93a2c
jurikjir <37962802+jurikjir@users.noreply.github.com>
2020-06-19 16:54:48 +0200

    Predict api

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def predict():
     prediction = int(prediction.Label[0])
     return render_template("page.htm", pred="Expected bill will be {}".format(prediction))
 
+@app.route('predict_api', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
